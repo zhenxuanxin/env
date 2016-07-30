8ee5310e8656ff0e8956c737b360acd39b12e290
zhenxxin <zhenxuanxin@gmail.com>
2016-07-30 13:25:13 +0800

    Add automation script

---
+++ shell/init.sh
@@ -0,0 +1,10 @@
+# clone repository
+git clone git@github.com:zhenxxin:env.git ~/Develop/env
+git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# link .vimrc
+ln -s ~/Develop/env/vim/.vimrc ~/.vimrc
+
+# install vim plugins
+vim +PluginInstall +qall
+
